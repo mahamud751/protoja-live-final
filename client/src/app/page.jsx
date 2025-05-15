3743ec933c7aea7e551b327fe727fae945f6e04f
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-05-16 03:38:04 +0600

    Updated

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,9 +1,4 @@
-import { Box, Container } from "@mui/material";
-import Faq from "@/components/home/Faq";
-import Question from "@/components/home/Question";
 import Company from "@/components/home/Company";
-import Contact from "@/components/home/Contact";
-import Trending from "@/components/home/Trending";
 import ExpertiseSection from "@/components/home/ExpertiseSection";
 import HeroSection from "@/components/home/HeroSection";
 import DesignForFutureSection from "@/components/home/DesignForFutureSection";
@@ -19,18 +14,10 @@ export default function Home() {
       <ExpertiseSection />
       <ProjectApproachSlider/>
       <DesignForFutureSection/>
-      {/* <Trending /> */}
       <Company />
       <StatsCounter/>
       <BlogSection/>
       <ProjectShowcase/>
-      {/* <Box className="w-full mt-20 px-3 sm:px-6 lg:px-16 bg-[#202124] p-16">
-        <Container>
-          <Faq />
-        </Container>
-      </Box>
-      <Question header={"Unlock your new design team today."} />
-      <Contact /> */}
     </>
   );
 }
